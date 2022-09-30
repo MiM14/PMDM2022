4a40504f8ff379682b46b4a3675fd52163c6be63
Ismael Martín Moakil <85709107+MiM14@users.noreply.github.com>
2022-09-30 20:13:14 +0200

    Ex02 and Starting Ex03

---
+++ app/src/main/java/com/moaimar/pmdm/ut01/ex02/presentation/Ut01Ex02Activity.kt
@@ -0,0 +1,12 @@
+package com.moaimar.pmdm.ut01.ex02.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.moaimar.pmdm.R
+
+class Ut01Ex02Activity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex02)
+    }
+}

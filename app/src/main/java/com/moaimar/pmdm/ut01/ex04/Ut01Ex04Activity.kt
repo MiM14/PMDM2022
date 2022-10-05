463753eb6d908b6ba0280205f266a061a7d280e9
Ismael Martín Moakil <85709107+MiM14@users.noreply.github.com>
2022-10-05 16:16:08 +0200

    Ex04

---
+++ app/src/main/java/com/moaimar/pmdm/ut01/ex04/Ut01Ex04Activity.kt
@@ -0,0 +1,12 @@
+package com.moaimar.pmdm.ut01.ex04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moaimar.pmdm.R
+
+class Ut01Ex04Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex04)
+    }
+}
